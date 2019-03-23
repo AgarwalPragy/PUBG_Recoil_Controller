b607774dceefc88144b0425c1fc1ae9f2a5626ca
Pragy Agarwal <agar.pragy@gmail.com>
2019-03-23 05:53:54 +0530

    Trial

--- src/config.py
+++ src/config.py
@@ -6,26 +6,14 @@ __all__ = ['mouse_poll_time', 'time_between_mouse_move', 'recoil_table', 'recoil
 debug = False
 
 enabled_anti_recoil = True
-enabled_ads_help = False
-# todo: ads helper really screws us over by turning on hip fire,
-#  or removing our ads, and the costs outweigh the benefits.
-#  It can be improved by detecting when we've been in ads
-#  for too long while moving or doing ads cancelling things.
-enabled_lean_help = False
-# todo: can be improved by detecting when an
-#  ads while previously active, and not changing the ads direction once set
-#  Currently, trying to get back to cover while firing while learning
-#  right, will cause the lean to shift to left which screws stuff up.
-
 
 enabled_limit_fire_time = False
+max_fire_time = 1
 
 main_loop_sleep_time = 0.01
 mouse_poll_time = 0.01
 time_between_mouse_move = 0.02
 keyboard_poll_time = 0.05
-movement_direction_decay_time = 0.3
-max_fire_time = 1
 burst_time = 0.3
 
 max_info_lines = 3
@@ -99,14 +87,17 @@ class Keys(IntEnum):
     primary_weapon   = 2   # key num 1
     secondary_weapon = 3   # key num 2
     sidearm          = 4   # key num 3
-    molotov          = 6   # key num 5
-    smoke_grenade    = 8   # key num 7
-    he_grenade       = 9   # key num 8
-    stun_grenade     = 10  # key num 9
 
-    throwables       = 34  # g
+    throwables       = 5   # key num 4
+    meelee           = 44  # z
     unarm            = 45  # x
-    use              = 7   # key num 6
+
+    use              = 102  # F15
+    use_redirect     = 25   # p
+    reload           = 19   # r
+
+    toggle_map       = 33  # f
+    toggle_inventory = 15  # tab
 
     lean_left        = 16  # q
     lean_right       = 18  # e
@@ -114,22 +105,20 @@ class Keys(IntEnum):
     strafe_right     = 32  # d
 
     hold_breath      = 42  # left shift
-    toggle_canted    = 59  # F1
-    alternate_ads    = 60  # F2
-    alternate_fire   = 61  # F1
 
-
-    toggle_map       = 33  # f
-    toggle_inventory = 15  # tab
-
-    reload           = 19  # r
+    alternate_ads    = 68  # F10
+    alternate_fire   = 87  # F11
 
 
 class HotKeys(IntEnum):
-    rotate_primary_zoom   = 12  # -
-    rotate_secondary_zoom = 13  # =
-    toggle_script         = 41  # `
-    reset_state           = 1   # esc
+    zoom_1x          = 59  # F1
+    zoom_2x          = 60  # F2
+    zoom_3x          = 61  # F3
+    zoom_4x          = 62  # F4
+    zoom_6x          = 64  # F6
+
+    toggle_script    = 41  # `
+    reset_state      = 1   # esc
 
 
 game_window_text = "PLAYERUNKNOWN'S BATTLEGROUNDS "
--- src/main.py
+++ src/main.py
@@ -18,55 +18,41 @@ from enums import *
 
 class GameState:
     active_weapon = Weapons.other
-    canted_active = False
     ads_active = False
     screen = Screens.play
-    primary_zoom = Zoom.x1
+    secondary_zoom = Zoom.x1
 
     holding_breath = False
     is_firing = False
 
     fire_start_time = None
-    applicable_strafe_direction = None
 
     @staticmethod
     def reset():
         GameState.active_weapon = Weapons.other
-        GameState.canted_active = False
         GameState.ads = False
         GameState.screen = Screens.play
-        GameState.primary_zoom = Zoom.x1
+        GameState.secondary_zoom = Zoom.x1
 
         GameState.holding_breath = False
         GameState.is_firing = False
 
         GameState.fire_start_time = None
-        GameState.applicable_strafe_direction = None
 
 
 class ScriptState:
     active = False
 
-    last_strafe_direction_time = None
     recoil_index = 0
-
-    ads_helping = False
-    lean_helping = False
-
     last_mouse_poll_time = None
     last_keyboard_poll_time = None
-    cross = None
+    crosshair_ = None
 
     @staticmethod
     def reset():
         ScriptState.active = False
 
-        ScriptState.last_strafe_direction_time = 0
         ScriptState.recoil_index = 0
-
-        ScriptState.ads_helping = False
-        ScriptState.lean_helping = False
-
         ScriptState.last_mouse_poll_time = 0
         ScriptState.last_keyboard_poll_time = 0
 
@@ -75,18 +61,16 @@ def update_ui():
     global last_info
     script = [termcolor.colored('◼', 'red'), termcolor.colored('▶', 'green')][ScriptState.active]
     weapon = [termcolor.colored('🔫', 'green'), termcolor.colored('🔪', 'red'), termcolor.colored('💣', 'red')][GameState.active_weapon.value]
-    canted = [termcolor.colored('⦻', 'green'), termcolor.colored('⛶', 'red')][GameState.canted_active]
     ads    = [termcolor.colored('⚪', 'green'), termcolor.colored('⯐', 'red')][GameState.ads_active]
     screen = [termcolor.colored('🌍', 'red'), termcolor.colored('📖', 'red'), termcolor.colored('🎮', 'green')][GameState.screen.value]
-    zoom1  =  termcolor.colored(f'{GameState.primary_zoom.name[1:]}x', ['green', 'blue', 'red', 'red', 'red'][GameState.primary_zoom.value])
+    zoom2  =  termcolor.colored(f'{GameState.secondary_zoom.name[1:]}x', ['green', 'blue', 'red', 'red', 'red'][GameState.secondary_zoom.value])
     breath = [termcolor.colored('👃', 'green'), termcolor.colored('👽', 'red')][GameState.holding_breath]
     firing = [termcolor.colored('😶', 'green'), termcolor.colored('⚡', 'red')][GameState.is_firing]
-    strafe = termcolor.colored('⊙', 'green') if GameState.applicable_strafe_direction is None else termcolor.colored(['⇦', '⇨'][GameState.applicable_strafe_direction.value], 'red')
     recoil_index = str(ScriptState.recoil_index).zfill(3)
     sep_start = termcolor.colored('[', 'magenta')
     sep_end = termcolor.colored(']', 'magenta')
     sep_mid = termcolor.colored('] [', 'magenta')
-    info = [sep_start, script, sep_mid, weapon, zoom1, canted, ads, sep_mid, screen, firing, strafe, breath, recoil_index, sep_end]
+    info = [sep_start, script, sep_mid, weapon, zoom2, ads, sep_mid, screen, firing, breath, recoil_index, sep_end]
     if info != last_info:
         sys.stdout.write('\r' + '  '.join(map(str, info)))
         sys.stdout.flush()
@@ -115,16 +99,6 @@ def poll_keyboard(ts):
     if ts - ScriptState.last_keyboard_poll_time < config.keyboard_poll_time:
         return
     ScriptState.last_keyboard_poll_time = ts
-
-    if keyboard.is_pressed(config.Keys.strafe_right):
-        GameState.applicable_strafe_direction = Movement.right
-        ScriptState.last_strafe_direction_time = ts
-    elif keyboard.is_pressed(config.Keys.strafe_left):
-        GameState.applicable_strafe_direction = Movement.left
-        ScriptState.last_strafe_direction_time = ts
-    elif ts - ScriptState.last_strafe_direction_time > config.movement_direction_decay_time:
-        GameState.applicable_strafe_direction = None
-
     GameState.holding_breath = keyboard.is_pressed(config.Keys.hold_breath)
 
 
@@ -142,20 +116,14 @@ def main_loop():
             continue
 
         if GameState.is_firing:
-            if config.enabled_ads_help:
-                if not GameState.ads_active and not ScriptState.ads_helping:
-                    keyboard.press_and_release(config.Keys.alternate_ads)
-                    GameState.canted_active = True
-                    ScriptState.ads_helping = True
-                    GameState.ads_active = True
-                    disable_crosshair()
-                    keyboard.press_and_release(config.Keys.toggle_canted)
             if config.enabled_anti_recoil and (ts - GameState.fire_start_time) >= ScriptState.recoil_index * config.time_between_mouse_move:
-                zoom = Zoom.x1 if GameState.canted_active else GameState.primary_zoom
-                if zoom == Zoom.x1 and GameState.holding_breath:
+                zoom = GameState.secondary_zoom if GameState.active_weapon == Weapons.secondary and GameState.ads_active else Zoom.x1
+                if zoom == Zoom.x1 and GameState.holding_breath and GameState.ads_active:
                     zoom = Zoom.xx
                 amount = config.recoil_table[ScriptState.recoil_index]
                 amount = int(amount * config.recoil_multipliers[zoom])
+                if GameState.active_weapon == Weapons.secondary:
+                    amount = int(amount * 0.8)
                 utils.in_game_mouse_move(amount)
                 ScriptState.recoil_index = ScriptState.recoil_index + 1
                 if ScriptState.recoil_index == len(config.recoil_table):
@@ -163,35 +131,10 @@ def main_loop():
                     mouse.release('left')
                     GameState.fire_start_time = None
 
-
-            if config.enabled_lean_help:
-                if GameState.applicable_strafe_direction == Movement.left and not ScriptState.lean_helping:
-                    keyboard.press(config.Keys.lean_left)
-                    ScriptState.lean_helping = True
-                elif GameState.applicable_strafe_direction is Movement.right and not ScriptState.lean_helping:
-                    keyboard.press(config.Keys.lean_right)
-                    ScriptState.lean_helping = True
-                else:
-                    # don't touch anything. We might have gone from strafing to leaned firing which caused the application direction
-                    # to vanish or change, so if we're already in a lean, we shouldn't cancel or change directions.
-                    pass
-
             if config.enabled_limit_fire_time:
                 if GameState.fire_start_time is not None and ts - GameState.fire_start_time > config.max_fire_time:
                     mouse.release('left')
         else:
-            if ScriptState.ads_helping:
-                keyboard.press_and_release(config.Keys.toggle_canted)
-                ScriptState.ads_helping = False
-                GameState.ads_active = False
-                GameState.canted_active = False
-                enable_crosshair()
-                keyboard.press_and_release(config.Keys.alternate_ads)
-            if ScriptState.lean_helping:
-                keyboard.release(config.Keys.lean_left)
-                keyboard.release(config.Keys.lean_right)
-                ScriptState.lean_helping = False
-
             ScriptState.recoil_index = 0
 
 
@@ -206,9 +149,9 @@ def reset_state():
     cancel_ads()
 
 
-def rotate_primary_zoom():
+def set_secondary_zoom(value: Zoom):
     if utils.is_game_in_foreground() or config.debug:
-        GameState.primary_zoom = Zoom((GameState.primary_zoom.value + 1) % 5)
+        GameState.secondary_zoom = value
 
 
 def toggle_inventory():
@@ -263,11 +206,11 @@ def cancel_ads():
 
 
 def enable_crosshair() -> None:
-    ScriptState.cross = reload_crosshair(ScriptState.cross)
+    ScriptState.crosshair_ = reload_crosshair(ScriptState.crosshair_)
 
 
 def disable_crosshair() -> None:
-    ScriptState.cross.allow_draw = False
+    ScriptState.crosshair_.allow_draw = False
 
 
 if __name__ == '__main__':
@@ -278,20 +221,24 @@ if __name__ == '__main__':
 
     keyboard.on_press_key(config.Keys.toggle_inventory, lambda _: toggle_inventory())
     keyboard.on_press_key(config.Keys.toggle_map, lambda _: toggle_map())
+    mouse.on_right_click(toggle_ads)
 
-    keyboard.on_press_key(config.HotKeys.rotate_primary_zoom, lambda _: rotate_primary_zoom())
+    keyboard.on_press_key(config.HotKeys.zoom_1x, lambda _: set_secondary_zoom(Zoom.x1))
+    keyboard.on_press_key(config.HotKeys.zoom_2x, lambda _: set_secondary_zoom(Zoom.x2))
+    keyboard.on_press_key(config.HotKeys.zoom_3x, lambda _: set_secondary_zoom(Zoom.x3))
+    keyboard.on_press_key(config.HotKeys.zoom_4x, lambda _: set_secondary_zoom(Zoom.x4))
+    keyboard.on_press_key(config.HotKeys.zoom_6x, lambda _: set_secondary_zoom(Zoom.x6))
 
     keyboard.on_press_key(config.Keys.primary_weapon, lambda _: set_weapon_primary())
-    mouse.on_right_click(toggle_ads)
     keyboard.on_press_key(config.Keys.secondary_weapon, lambda _: set_weapon_secondary())
     keyboard.on_press_key(config.Keys.sidearm, lambda _: set_weapon_other())
     keyboard.on_press_key(config.Keys.throwables, lambda _: set_weapon_other())
     keyboard.on_press_key(config.Keys.unarm, lambda _: set_weapon_other())
-    keyboard.on_press_key(config.Keys.he_grenade, lambda _: set_weapon_other())
-    keyboard.on_press_key(config.Keys.stun_grenade, lambda _: set_weapon_other())
-    keyboard.on_press_key(config.Keys.molotov, lambda _: set_weapon_other())
-    keyboard.on_press_key(config.Keys.smoke_grenade, lambda _: set_weapon_other())
+    keyboard.on_press_key(config.Keys.meelee, lambda _: set_weapon_other())
+
     keyboard.on_press_key(config.Keys.reload, lambda _: cancel_ads())
+    keyboard.on_press_key(config.Keys.use, lambda _: keyboard.press_and_release(config.Keys.use_redirect))
+
 
     reset_state()
 

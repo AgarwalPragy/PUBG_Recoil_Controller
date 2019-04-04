2d2011e9a32c4c9b97f2aff2d1883791f651d5ad
Pragy Agarwal <agar.pragy@gmail.com>
2019-04-05 04:20:06 +0530

    Add horizontal recoil. Add recoil table for AKM. Add manual recoil multiplier. Refactor

--- src/utils.py
+++ src/utils.py
@@ -7,10 +7,15 @@ def is_game_in_foreground() -> bool:
     return config.game_window_text == win32gui.GetWindowText(win32gui.GetForegroundWindow())
 
 
+index = 0
+
+
 def in_game_mouse_move(y_relative: int):
+    global index
+    index = (index + 1) % 9
     windll.user32.mouse_event(
         c_uint(0x0001),
-        c_uint(0),
+        c_uint(int(index%2 == 1)),  # 4 / 9 times
         c_uint(y_relative),
         c_uint(0),
         c_uint(0)
